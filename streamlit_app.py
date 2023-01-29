e43afc92bd959e26106e75f05bd5cac257c61a93
Christopher Enam Franklin <cef1911@hotmail.com>
2023-01-29 15:59:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,9 @@ import pandas as pd
 import streamlit as st
 from PIL import Image
 
+image = Image.open('sunrise.jpg')
 
+st.image(image, caption='Sunrise by the mountains')
 
 """
 # Welcome to Streamlit!
